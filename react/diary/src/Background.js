fa168cb1924ba108a13a0adcade443697619caf0
David Dal Busco <david.dalbusco@outlook.com>
2023-02-19 19:42:56 +0100

    style: fix react

--- react/diary/src/Background.js
+++ react/diary/src/Background.js
@@ -7,7 +7,7 @@ export const Background = () => {
       >
         <path
           fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
-          fill-opacity=".3"
+          fillOpacity=".3"
           d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
         />
         <defs>
@@ -19,8 +19,8 @@ export const Background = () => {
             y2="474.645"
             gradientUnits="userSpaceOnUse"
           >
-            <stop stop-color="#9089FC" />
-            <stop offset="1" stop-color="#FF80B5" />
+            <stop stopColor="#9089FC" />
+            <stop offset="1" stopColor="#FF80B5" />
           </linearGradient>
         </defs>
       </svg>
