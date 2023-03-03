24f15bcbd7b8f23aacd37a8b30d8f177c4ed1857
David Dal Busco <david.dalbusco@outlook.com>
2023-03-03 16:48:25 +0100

    build: serve locally 5174

--- vanilla/vite.config.ts
+++ vanilla/vite.config.ts
@@ -3,5 +3,8 @@ import viteCompression from 'vite-plugin-compression';
 export default () => {
     return {
         plugins: [viteCompression()],
+        server: {
+            port: 5174
+        }
     };
 };
