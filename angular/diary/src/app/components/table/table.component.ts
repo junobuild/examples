6ab5250e6df4f1f977e16930d9a0189431e1e721
David Dal Busco <david.dalbusco@outlook.com>
2023-04-16 19:00:34 +0200

    feat: url

--- angular/diary/src/app/components/table/table.component.ts
+++ angular/diary/src/app/components/table/table.component.ts
@@ -5,16 +5,17 @@ import { type Doc } from '@junobuild/core';
 import { Observable } from 'rxjs';
 import { DocsService } from '../../services/docs.service';
 import type { Entry } from '../../types/entry';
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
-  imports: [BrowserModule, MatTableModule],
+  imports: [BrowserModule, MatTableModule, MatIconModule],
   standalone: true,
 })
 export class TableComponent {
-  displayedColumns: string[] = ['key', 'text'];
+  displayedColumns: string[] = ['key', 'text', 'url'];
 
   docs$: Observable<Doc<Entry>[]> = this.docsService.docs$;
 
