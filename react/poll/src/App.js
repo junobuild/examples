e078c26b682e4103e7156e3fb027e3e416b22833
David Dal Busco <david.dalbusco@outlook.com>
2023-04-27 14:23:12 +0200

    feat: avoid flickering

--- react/poll/src/App.js
+++ react/poll/src/App.js
@@ -1,15 +1,20 @@
 import { initJuno } from "@junobuild/core";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Auth } from "./Auth";
 import { Background } from "./Background";
 import { Poll } from "./Poll";
 
 function App() {
+  const [ready, setReady] = useState(false);
+
   useEffect(() => {
-    (async () =>
+    (async () => {
       await initJuno({
         satelliteId: "y7m4b-fiaaa-aaaal-acgna-cai",
-      }))();
+      });
+
+      setReady(true);
+    })();
   }, []);
 
   return (
@@ -19,25 +24,29 @@ function App() {
           <div className="relative px-6 pb-20 lg:px-8">
             <div className="mx-auto max-w-2xl pt-16 relative z-10">
               <div className="text-center">
-                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                  Did you like David's presentation?
-                </h1>
-                <p className="mt-6 text-lg leading-8 text-gray-600">
-                  A live polling app build with{" "}
-                  <a
-                    href="https://juno.build"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    className="underline"
-                  >
-                    Juno
-                  </a>
-                  .
-                </p>
+                {ready ? (
+                  <>
+                    <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                      Did you like David's presentation?
+                    </h1>
+                    <p className="mt-6 text-lg leading-8 text-gray-600">
+                      A live polling app build with{" "}
+                      <a
+                        href="https://juno.build"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        className="underline"
+                      >
+                        Juno
+                      </a>
+                      .
+                    </p>
 
-                <Auth>
-                  <Poll />
-                </Auth>
+                    <Auth>
+                      <Poll />
+                    </Auth>
+                  </>
+                ) : undefined}
               </div>
             </div>
             <Background />
