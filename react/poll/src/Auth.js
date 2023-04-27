9da313114a3fc52c07f6672135cd726f0c09850b
David Dal Busco <david.dalbusco@outlook.com>
2023-04-27 13:15:25 +0200

    feat: spinner

--- react/poll/src/Auth.js
+++ react/poll/src/Auth.js
@@ -2,11 +2,13 @@ import { authSubscribe } from "@junobuild/core";
 import { createContext, useEffect, useState } from "react";
 import { Login } from "./Login";
 import { Logout } from "./Logout";
+import { Spinner } from "./Spinner";
 
 export const AuthContext = createContext();
 
 export const Auth = ({ children }) => {
   const [user, setUser] = useState(undefined);
+  const [busy, setBusy] = useState(undefined);
 
   useEffect(() => {
     const sub = authSubscribe((user) => setUser(user));
@@ -15,7 +17,7 @@ export const Auth = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user }}>
+    <AuthContext.Provider value={{ user, setBusy }}>
       {user !== undefined && user !== null ? (
         <div>
           {children}
@@ -25,6 +27,8 @@ export const Auth = ({ children }) => {
       ) : (
         <Login />
       )}
+
+      {busy ? <Spinner /> : undefined}
     </AuthContext.Provider>
   );
 };
--- react/poll/src/Login.js
+++ react/poll/src/Login.js
@@ -1,21 +1,41 @@
 import { NFIDProvider, signIn } from "@junobuild/core";
 import { IconII } from "./IconII";
 import { IconNFID } from "./IconNFID";
+import { useContext } from "react";
+import { AuthContext } from "./Auth";
 
 export const Login = () => {
-  const signInNFID = () =>
-    signIn({
-      provider: new NFIDProvider({
-        appName: "David presentation",
-        logoUrl: "https://somewhere.com/your_logo.png",
-      }),
-    });
+  const { setBusy } = useContext(AuthContext);
+
+  const login = async (signIn) => {
+    setBusy(true);
+
+    try {
+      await signIn();
+    } catch (err) {
+      console.error(err);
+    }
+
+    setBusy(false);
+  };
+
+  const signInII = async () => login(async () => signIn());
+
+  const signInNFID = async () =>
+    login(async () =>
+      signIn({
+        provider: new NFIDProvider({
+          appName: "David presentation",
+          logoUrl: "https://somewhere.com/your_logo.png",
+        }),
+      })
+    );
 
   return (
     <div className="mt-10 flex flex-col items-center justify-center gap-3 max-w-sm mx-auto">
       <button
         type="button"
-        onClick={signIn}
+        onClick={signInII}
         className="w-full rounded-md bg-black border-2 border-black hover:border-indigo-500 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       >
         <div className="flex items-center justify-center gap-1.5">
