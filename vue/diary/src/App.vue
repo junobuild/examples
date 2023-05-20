ffccde1e55fa80a53acfc9fece547ecab3c78f4c
David Dal Busco <david.dalbusco@outlook.com>
2023-05-20 08:59:35 -0400

    feat: vuejs example

---
+++ vue/diary/src/App.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import Auth from './components/Auth.vue'
+import Background from './components/Background.vue'
+import Table from '@/components/Table.vue'
+import { onMounted } from 'vue'
+import { initJuno } from '@junobuild/core'
+
+onMounted(
+  async () =>
+    await initJuno({
+      satelliteId: 'st75y-vaaaa-aaaaa-aaalq-cai',
+      localIdentityCanisterId: 'rrkah-fqaaa-aaaaa-aaaaq-cai'
+    })
+)
+</script>
+
+<template>
+  <div class="isolate bg-white">
+    <main>
+      <div class="relative px-6 lg:px-8">
+        <div class="mx-auto max-w-2xl pt-16">
+          <div class="text-center">
+            <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Sample Juno App
+            </h1>
+            <p class="mt-6 text-lg leading-8 text-gray-600">
+              A sample app build with Vue, Tailwind and
+              <a
+                href="https://juno.build"
+                rel="noopener noreferrer"
+                target="_blank"
+                class="underline"
+              >
+                Juno
+              </a>
+              .
+            </p>
+
+            <Auth>
+              <Table />
+            </Auth>
+
+            <Background />
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
---
+++ vue/diary/src/components/Table.vue
@@ -0,0 +1,55 @@
+<script lang="ts" setup>
+import { listDocs } from '@junobuild/core'
+import { onMounted, ref } from 'vue'
+
+const items = ref([])
+
+const list = async () => {
+  const { items } = await listDocs({
+    collection: 'notes',
+    filter: {}
+  })
+
+  items.value = items
+}
+
+onMounted(async () => await list())
+</script>
+
+<template>
+  <div class="w-full max-w-2xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200 mt-8">
+    <header class="px-5 py-4 border-b border-gray-100">
+      <h2 class="font-semibold text-gray-800">Entries</h2>
+    </header>
+    <div class="p-3">
+      <div class="overflow-x-auto">
+        <div class="flex items-center gap-6 px-2.5 py-1.5" v-for="(item, index) in items">
+          <span
+            class="px-4 py-2 rounded-full text-gray-500 bg-gray-200 font-semibold text-sm flex align-center w-max"
+          >
+            {{ index + 1 }}
+          </span>
+          <div class="line-clamp-3 text-left grow">{{ item.text }}</div>
+          <div v-if="item.url !== undefined">
+            <a aria-label="Open data" rel="noopener noreferrer" href="{{item.url}}" target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
