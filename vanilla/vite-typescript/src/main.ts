99d2ee33d467b0063eff8300e7c93922925ee43b
David Dal Busco <david.dalbusco@outlook.com>
2023-09-29 08:34:30 +0200

    feat: add orbiter / analytics to vanilla sample (#9)
    * feat: add orbiter / analytics to vanilla sample
    
    * feat: metadata for track event
    
    * feat: europe
    
    * feat: update ids
    
    * feat: update ids
    
    * feat: update ids
    
    * build: install lib

--- vanilla/vite-typescript/src/main.ts
+++ vanilla/vite-typescript/src/main.ts
@@ -1,22 +1,73 @@
-import { authSubscribe, initJuno, signIn, signOut, type User } from "@junobuild/core";
+import {
+  authSubscribe,
+  initJuno,
+  signIn,
+  signOut,
+  type User,
+} from "@junobuild/core";
 import "./style.css";
-import {initDoc} from "./doc";
-import {initStorage} from "./storage";
+import { initDoc } from "./doc";
+import { initStorage } from "./storage";
+import { initOrbiter, trackEvent } from "@junobuild/analytics";
 
-document.addEventListener("DOMContentLoaded", async () => await initJuno({
-  satelliteId: "xo2hm-lqaaa-aaaal-ab3oa-cai",
-  workers: {
-    auth: true
-  }
-}), {once: true});
+document.addEventListener(
+  "DOMContentLoaded",
+  async () => {
+    await Promise.all([
+      initJuno({
+        satelliteId: "hbrpn-74aaa-aaaaa-qaaxq-cai",
+        localIdentityCanisterId: "rrkah-fqaaa-aaaaa-aaaaq-cai",
+        workers: {
+          auth: true,
+        },
+      }),
+      initOrbiter({
+        satelliteId: "hbrpn-74aaa-aaaaa-qaaxq-cai",
+        orbiterId: "f3nhr-bmaaa-aaaaa-qaayq-cai",
+        env: "dev",
+      }),
+    ]);
+  },
+  { once: true }
+);
+
+document
+  .querySelector("#push")
+  ?.addEventListener(
+    "click",
+    () => history.pushState(null, "", "nested/index.html"),
+    { passive: true }
+  );
+
+document.querySelector("#track")?.addEventListener(
+  "click",
+  async () => {
+    await trackEvent({
+      name: "Yolo yolo 2",
+      metadata: {
+        hello: "world world",
+        hello1: "world world",
+        hello2: "world world",
+        hello3: "world world",
+        hello4: "world world",
+        hello5: "world world",
+        hello6: "world world",
+        hello7: "world world",
+        hello8: "world world",
+        hello9: "world world",
+      },
+    });
+  },
+  { passive: true }
+);
 
 document
   .querySelector("#signin")
   ?.addEventListener("click", async () => await signIn(), { passive: true });
 
 document
-    .querySelector("#signout")
-    ?.addEventListener("click", signOut, { passive: true });
+  .querySelector("#signout")
+  ?.addEventListener("click", signOut, { passive: true });
 
 authSubscribe((user: User | null) => {
   console.log("User", user);
