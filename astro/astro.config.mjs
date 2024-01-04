6bbaa0ac048a55cab269ddb8b8f0b6e1004623ae
David Dal Busco <david.dalbusco@outlook.com>
2024-01-04 14:42:21 +0100

    feat: juno docker example

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
+import juno from "@junobuild/vite-plugin";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nkzsw-gyaaa-aaaal-ada3a-cai.icp0.io",
   integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [juno({
+      container: true
+    })],
+  },
 });
