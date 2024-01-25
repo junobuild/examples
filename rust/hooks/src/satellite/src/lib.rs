d7a2a004adc6a1a501bcb2c0dc19d7f425a1f718
David Dal Busco <david.dalbusco@outlook.com>
2024-01-25 17:59:29 +0100

    feat: Rust hooks example

---
+++ rust/hooks/src/satellite/src/lib.rs
@@ -0,0 +1,30 @@
+use junobuild_macros::{on_delete_doc, on_delete_many_docs, on_set_doc, on_set_many_docs};
+use junobuild_satellite::{
+    include_satellite, OnDeleteDocContext, OnDeleteManyDocsContext, OnSetDocContext,
+    OnSetManyDocsContext,
+};
+use ic_cdk::print;
+
+#[on_set_doc]
+async fn on_set_doc(_context: OnSetDocContext) -> Result<(), String> {
+    print("Hi, how are you doing?");
+
+    Ok(())
+}
+
+#[on_set_many_docs]
+async fn on_set_many_docs(_context: OnSetManyDocsContext) -> Result<(), String> {
+    Ok(())
+}
+
+#[on_delete_doc]
+async fn on_delete_doc(_context: OnDeleteDocContext) -> Result<(), String> {
+    Ok(())
+}
+
+#[on_delete_many_docs]
+async fn on_delete_many_docs(_context: OnDeleteManyDocsContext) -> Result<(), String> {
+    Ok(())
+}
+
+include_satellite!();
