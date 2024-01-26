acd949ffc394e39ecaa5b0cab57d987e6ea2db9b
David Dal Busco <david.dalbusco@outlook.com>
2024-01-26 13:11:28 +0100

    feat: update example

--- rust/hooks/src/satellite/src/lib.rs
+++ rust/hooks/src/satellite/src/lib.rs
@@ -1,14 +1,48 @@
+use ic_cdk::print;
 use junobuild_macros::{on_delete_doc, on_delete_many_docs, on_set_doc, on_set_many_docs};
 use junobuild_satellite::{
-    include_satellite, OnDeleteDocContext, OnDeleteManyDocsContext, OnSetDocContext,
-    OnSetManyDocsContext,
+    include_satellite, set_doc_store, OnDeleteDocContext, OnDeleteManyDocsContext, OnSetDocContext,
+    OnSetManyDocsContext, SetDoc,
 };
-use ic_cdk::print;
+use junobuild_utils::{decode_doc_data, encode_doc_data};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Person {
+    yolo: bool,
+    hello: String,
+}
 
 #[on_set_doc(collections = ["demo"])]
 async fn on_set_doc(context: OnSetDocContext) -> Result<(), String> {
-    print("Hi, how are you doing?");
-    print(format!("{:?}", context.data.data.after.owner));
+    let mut data: Person = decode_doc_data(&context.data.data.after.data)?;
+
+    print(format!("[on_set_doc] Caller: {}", context.caller.to_text()));
+
+    print(format!(
+        "[on_set_doc] Collection: {}",
+        context.data.collection
+    ));
+
+    print(format!("[on_set_doc] Data: {} {}", data.hello, data.yolo));
+
+    data.hello = format!("{} checked", data.hello);
+    data.yolo = false;
+
+    let encode_data = encode_doc_data(&data)?;
+
+    let doc: SetDoc = SetDoc {
+        data: encode_data,
+        description: context.data.data.after.description,
+        updated_at: Some(context.data.data.after.updated_at),
+    };
+
+    set_doc_store(
+        context.caller,
+        context.data.collection,
+        context.data.key,
+        doc,
+    )?;
 
     Ok(())
 }
