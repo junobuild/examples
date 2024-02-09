b404af34d5a834f7afcb490d10dcd3c48bca14d9
David Dal Busco <david.dalbusco@outlook.com>
2024-02-09 11:14:17 +0100

    docs: few comments

--- rust/hooks/src/satellite/src/lib.rs
+++ rust/hooks/src/satellite/src/lib.rs
@@ -12,6 +12,7 @@ use junobuild_utils::{decode_doc_data, encode_doc_data};
 use junobuild_utils::{DocDataBigInt, DocDataPrincipal};
 use serde::{Deserialize, Serialize};
 
+// The data of the document we are looking to update in the Satellite's Datastore.
 #[derive(Serialize, Deserialize)]
 struct Person {
     yolo: bool,
@@ -22,8 +23,10 @@ struct Person {
 
 #[on_set_doc(collections = ["demo"])]
 async fn on_set_doc(context: OnSetDocContext) -> Result<(), String> {
+    // We decode the new data saved in the Datastore because it holds those as blob.
     let mut data: Person = decode_doc_data(&context.data.data.after.data)?;
 
+    // Some printout for demo purpose
     print(format!("[on_set_doc] Caller: {}", context.caller.to_text()));
 
     print(format!(
@@ -36,17 +39,21 @@ async fn on_set_doc(context: OnSetDocContext) -> Result<(), String> {
         data.principal.value, data.value.value
     ));
 
+    // We update the document's data that was saved in the Datastore with the call from the frontend dapp.
     data.hello = format!("{} checked", data.hello);
     data.yolo = false;
 
+    // We encode the data back to blob.
     let encode_data = encode_doc_data(&data)?;
 
+    // We construct the parameters required to call the function that save the data in the Datastore.
     let doc: SetDoc = SetDoc {
         data: encode_data,
         description: context.data.data.after.description,
         updated_at: Some(context.data.data.after.updated_at),
     };
 
+    // We save the document for the same caller as the one who triggered the original on_set_doc, in the same collection with the same key as well.
     set_doc_store(
         context.caller,
         context.data.collection,
