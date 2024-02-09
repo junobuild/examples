5957661c459fe5260a9beee9da0533dc46f7560e
David Dal Busco <david.dalbusco@outlook.com>
2024-02-09 08:05:21 +0100

    feat: comments and scope to dogs collection

--- rust/https-outcalls/src/satellite/src/lib.rs
+++ rust/https-outcalls/src/satellite/src/lib.rs
@@ -1,28 +1,43 @@
+use ic_cdk::api::management_canister::http_request::{
+    http_request as http_request_out, CanisterHttpRequestArgument, HttpMethod,
+};
 use junobuild_macros::{
     on_delete_asset, on_delete_doc, on_delete_many_assets, on_delete_many_docs, on_set_doc,
     on_set_many_docs, on_upload_asset,
 };
-use junobuild_satellite::{include_satellite, OnDeleteAssetContext, OnDeleteDocContext, OnDeleteManyAssetsContext, OnDeleteManyDocsContext, OnSetDocContext, OnSetManyDocsContext, OnUploadAssetContext, set_doc_store, SetDoc};
-use ic_cdk::api::management_canister::http_request::{
-    http_request as http_request_out, CanisterHttpRequestArgument, HttpMethod, HttpResponse,
+use junobuild_satellite::{
+    include_satellite, set_doc_store, OnDeleteAssetContext, OnDeleteDocContext,
+    OnDeleteManyAssetsContext, OnDeleteManyDocsContext, OnSetDocContext, OnSetManyDocsContext,
+    OnUploadAssetContext, SetDoc,
 };
-use ic_cdk::print;
-use junobuild_utils::{encode_doc_data};
-use serde::{Serialize, Deserialize};
+use junobuild_utils::encode_doc_data;
+use serde::{Deserialize, Serialize};
 
+// The data of the document we are looking to update in the Satellite's Datastore.
 #[derive(Serialize, Deserialize)]
-struct Dog {
+struct DogData {
     src: Option<String>,
 }
 
+// We are using the Dog CEO API in this example.
+// https://dog.ceo/dog-api/
+//
+// Its endpoint "random" returns such JSON data:
+// {
+//     "message": "https://images.dog.ceo/breeds/mountain-swiss/n02107574_1118.jpg",
+//     "status": "success"
+// }
+//
+// That's why we declare a struct that matches the structure of the answer.
 #[derive(Serialize, Deserialize)]
 struct DogApiResponse {
     message: String,
     status: String,
 }
 
-#[on_set_doc]
+#[on_set_doc(collections = ["dogs"])]
 async fn on_set_doc(context: OnSetDocContext) -> Result<(), String> {
+    // 1. Prepare the HTTP GET request
     let url = "https://dog.ceo/api/breeds/image/random".to_string();
 
     let request_headers = vec![];
@@ -32,31 +47,39 @@ async fn on_set_doc(context: OnSetDocContext) -> Result<(), String> {
         method: HttpMethod::GET,
         body: None,
         max_response_bytes: None,
+        // In this simple example we skip sanitizing the response with a custom function for simplicity reason.
         transform: None,
+        // We do not require any particular HTTP headers in this example.
         headers: request_headers,
     };
 
+    // 2. Execute the HTTP request. A request consumes Cycles(!). In this example we provide 2_000_000_000 Cycles (= 0.002 TCycles).
     match http_request_out(request, 2_000_000_000).await {
         Ok((response,)) => {
+            // 3. Use serde_json to transform the response to a structured object.
             let str_body = String::from_utf8(response.body)
                 .expect("Transformed response is not UTF-8 encoded.");
 
-            print(format!("Response -------------> {}", str_body));
+            let dog_response: DogApiResponse =
+                serde_json::from_str(&str_body).map_err(|e| e.to_string())?;
 
-            let dog_response: DogApiResponse = serde_json::from_str(&str_body).map_err(|e| e.to_string())?;
-
-            let dog: Dog = Dog {
-                src: Some(dog_response.message)
+            // 4. Our goal is to update the document in the Datastore with an update that contains the link to the image fetched from the API we just called.
+            // That's we create such a data.
+            let dog: DogData = DogData {
+                src: Some(dog_response.message),
             };
 
+            // 5. We encode those data back to blob because the Datastore holds data as blob.
             let encode_data = encode_doc_data(&dog)?;
 
+            // 6. Then we construct the parameters required to call the function that save the data in the Datastore.
             let doc: SetDoc = SetDoc {
                 data: encode_data,
                 description: context.data.data.after.description,
                 updated_at: Some(context.data.data.after.updated_at),
             };
 
+            // 7. We store the data in the Datastore for the same caller as the one who triggered the original on_set_doc, in the same collection with the same key as well.
             set_doc_store(
                 context.caller,
                 context.data.collection,
@@ -65,7 +88,7 @@ async fn on_set_doc(context: OnSetDocContext) -> Result<(), String> {
             )?;
 
             Ok(())
-        },
+        }
         Err((r, m)) => {
             let message =
                 format!("The http_request resulted into error. RejectionCode: {r:?}, Error: {m}");
@@ -77,7 +100,6 @@ async fn on_set_doc(context: OnSetDocContext) -> Result<(), String> {
 
 #[on_set_many_docs]
 async fn on_set_many_docs(_context: OnSetManyDocsContext) -> Result<(), String> {
-
     Ok(())
 }
 
