68b536a39e74bb3e9db113dddcdd90434267e2a3
David Dal Busco <david.dalbusco@outlook.com>
2024-02-24 09:52:12 +0100

    build: use Juno JS config

---
+++ astro/juno.config.mjs
@@ -0,0 +1,33 @@
+import { defineConfig } from "@junobuild/config";
+
+/** @type {import('@junobuild/config').JunoConfig} */
+export default defineConfig({
+  satellite: {
+    satelliteId: "nkzsw-gyaaa-aaaal-ada3a-cai",
+    source: "dist",
+    storage: {
+      headers: [
+        {
+          source: "**/*.jpg",
+          headers: [
+            ["Cache-Control", "max-age=31536000"],
+            ["Access-Control-Allow-Origin", "*"],
+          ],
+        },
+      ],
+      redirects: [
+        {
+          source: "/blog/redirect",
+          location: "/blog/first-post/index.html",
+          code: 301,
+        },
+      ],
+      rewrites: [
+        {
+          source: "/blog/**",
+          destination: "/blog/first-post/index.html",
+        },
+      ],
+    },
+  },
+});
