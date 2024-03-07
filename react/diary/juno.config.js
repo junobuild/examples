f6a3b0014b24d037fa9e980fab69a1d0b262cc59
David Dal Busco <david.dalbusco@outlook.com>
2024-03-07 13:11:19 +0100

    build: js config file

---
+++ react/diary/juno.config.js
@@ -0,0 +1,9 @@
+import {defineConfig} from '@junobuild/config';
+
+/** @type {import('@junobuild/config').JunoConfig} */
+export default defineConfig({
+  satellite: {
+    id: 'f62k6-laaaa-aaaal-acq7q-cai',
+    source: 'dist'
+  }
+});
