19d46dce3f818a6e79b2e013240c13bed34834e8
David Dal Busco <david.dalbusco@outlook.com>
2024-03-07 18:08:50 +0100

    feat: button

---
+++ react/diary/src/Button.jsx
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+
+export const Button = ({ children, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="flex items-center gap-2 border-black border-2 transition-all rounded-none py-1 px-8 font-semibold text-white bg-lavender-blue-500 hover:bg-lavender-blue-600 active:bg-lavender-blue-400 shadow-[5px_5px_0px_rgba(0,0,0,1)] active:shadow-none active:translate-x-[5px] active:translate-y-[5px]"
+    >
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
--- react/diary/tailwind.config.js
+++ react/diary/tailwind.config.js
@@ -1,11 +1,35 @@
+import { fontFamily } from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif", ...fontFamily.sans],
+    },
     extend: {
       screens: {
-        'tall': { 'raw': '(min-height: 800px)' },
-      }
+        tall: { raw: "(min-height: 800px)" },
+      },
+    },
+    colors: {
+      inherit: "inherit",
+      transparent: "transparent",
+      current: "currentColor",
+      black: "rgb(0 0 0)",
+      white: "rgb(255 255 255)",
+      ["lavender-blue"]: {
+        50: "#f2f3ff",
+        100: "#e4e7ff",
+        200: "#c9cfff",
+        300: "#aeb8ff",
+        400: "#93a0ff",
+        500: "#7888ff",
+        600: "#606dcc",
+        700: "#485299",
+        800: "#303666",
+        900: "#181b33",
+      },
     },
   },
   plugins: [],
