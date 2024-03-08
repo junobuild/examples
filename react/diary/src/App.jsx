593bd52c855795ab9ba870a6540f879bd60b0615
David Dal Busco <david.dalbusco@outlook.com>
2024-03-08 07:51:59 +0100

    feat: footer

--- react/diary/src/App.jsx
+++ react/diary/src/App.jsx
@@ -4,6 +4,7 @@ import { Modal } from "./Modal";
 import { initJuno } from "@junobuild/core";
 import { Auth } from "./Auth";
 import { useEffect } from "react";
+import {Footer} from "./Footer";
 
 function App() {
   useEffect(() => {
@@ -17,8 +18,8 @@ function App() {
   return (
     <>
       <div className="relative isolate bg-white h-[100dvh]">
-        <main className="mx-auto max-w-screen-2xl p-16 md:py-48 md:px-24">
-          <h1 className="text-6xl font-bold tracking-tight">Sample Juno App</h1>
+        <main className="mx-auto max-w-screen-2xl p-16 md:p-24 tall:h-[calc(100dvh-96px)]">
+          <h1 className="text-5xl md:text-6xl font-bold tracking-tight md:pt-24">Sample Juno App</h1>
           <p className="mt-6 text-lg leading-8 text-gray-600">
             A sample app build with React, Tailwind and{" "}
             <a
@@ -38,6 +39,8 @@ function App() {
             <Modal />
           </Auth>
         </main>
+
+        <Footer />
 
         <Background />
       </div>
