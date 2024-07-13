2840f882d70e0cd9ffd15a2de76326df7bc99e64
David Dal Busco <david.dalbusco@outlook.com>
2024-07-13 08:37:31 +0200

    feat: continue implementation

--- rust/json/src/satellite/src/lib.rs
+++ rust/json/src/satellite/src/lib.rs
@@ -3,14 +3,52 @@ use junobuild_macros::{
     on_delete_doc, on_delete_many_assets, on_delete_many_docs, on_set_doc, on_set_many_docs,
     on_upload_asset,
 };
-use junobuild_satellite::{
-    include_satellite, AssertDeleteAssetContext, AssertDeleteDocContext, AssertSetDocContext,
-    AssertUploadAssetContext, OnDeleteAssetContext, OnDeleteDocContext, OnDeleteManyAssetsContext,
-    OnDeleteManyDocsContext, OnSetDocContext, OnSetManyDocsContext, OnUploadAssetContext,
-};
+use junobuild_satellite::{include_satellite, AssertDeleteAssetContext, AssertDeleteDocContext, AssertSetDocContext, AssertUploadAssetContext, OnDeleteAssetContext, OnDeleteDocContext, OnDeleteManyAssetsContext, OnDeleteManyDocsContext, OnSetDocContext, OnSetManyDocsContext, OnUploadAssetContext, set_doc_store, SetDoc};
+use serde::{Deserialize, Serialize};
+use junobuild_utils::{decode_doc_data, encode_doc_data};
+
+#[derive(Serialize, Deserialize)]
+struct Note {
+    text: String,
+    url: Option<String>
+}
 
 #[on_set_doc]
-async fn on_set_doc(_context: OnSetDocContext) -> Result<(), String> {
+async fn on_set_doc(context: OnSetDocContext) -> Result<(), String> {
+    ic_cdk::print("Let's go!");
+
+    let mut note: Note = decode_doc_data(&context.data.data.after.data)?;
+
+    note.text = format!("{} checked.", note.text);
+
+    let encode_data = encode_doc_data(&note)?;
+
+    let doc: SetDoc = SetDoc {
+        data: encode_data,
+        description: context.data.data.after.description,
+        version: context.data.data.after.version,
+    };
+
+    set_doc_store(
+        context.caller,
+        context.data.collection,
+        context.data.key,
+        doc,
+    )?;
+
+    // We want to build a JSON file
+    // No problemo.
+
+    // We want to upload the JSON file to the memory so that it becomes
+    // Available on the web
+    // 1. Set the json file directly
+
+    // create_asset_with_content(Asset {
+    //     full_path: "/myfile.json",
+    //     content: json_for_the_note(s)
+    // })
+
+
     Ok(())
 }
 
