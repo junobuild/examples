4ddba1d8e39150151387b3bc7ed7b54dc1dafc60
David Dal Busco <david.dalbusco@outlook.com>
2025-01-28 12:55:36 +0100

    chore: update

--- node/admin/controller/index.mjs
+++ node/admin/controller/index.mjs
@@ -2,7 +2,6 @@
 
 import { getIdentity } from "./auth.mjs";
 import { setSatellitesController } from "@junobuild/admin";
-import fetch from "node-fetch";
 import { Principal } from "@dfinity/principal";
 
 const identity = getIdentity();
@@ -10,7 +9,6 @@ const identity = getIdentity();
 const missionControl = {
   identity,
   missionControlId: process.env.JUNO_MISSION_CONTROL_ID,
-  fetch,
 };
 
 const satelliteId = process.env.JUNO_SATELLITE_ID;
